2ba6a017dffa94a51d3b11aed47fd096b9900f08
Shrea15 <bshreya834@gmail.com>
2024-04-11 11:16:57 +0530

    Api

---
+++ E-Commerce-Application/src/main/java/com/retail/e_com/ECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.retail.e_com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplication.class, args);
+	}
+
+}
